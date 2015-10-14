f994bdf64130eac9869f1d633a81206b63284662
Jason D Todd <jdt34@duke.edu>
2015-10-13 23:09:48 -0400

    Update get_lq.R
    one minute!

--- get_lq.R
+++ get_lq.R
@@ -78,11 +78,11 @@ scrape_list = function(page) {
 html_download = function(link) {
   download.file(link, destfile=paste0(to_put, basename(link)), quiet=T)
   
-  # Direct the computer to rest for thirty seconds after downloading the webpage.
-  Sys.sleep(30)
+  # Direct the computer to rest for one minute after downloading the webpage.
+  Sys.sleep(60)
 }
 
 
-# Download the webpage for each La Quinta hotel in the US, at a rate of one every 30 seconds.
+# Download the webpage for each La Quinta hotel in the US, at a rate of one per minute.
 # l_ply takes each element of a list, apply function and discard results
 scrape_list(page) %>% l_ply(.fun=html_download)
